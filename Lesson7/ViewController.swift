941c9cdce369d318aa070a3590c2cb7b71f84eab
dima <dima@kojem.ru>
2023-10-01 02:46:33 +0300

    Initial Commit

---
+++ Lesson7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson7
+//
+//  Created by Дима Кожемякин on 01.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
